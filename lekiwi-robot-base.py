e58ef0f34a3247daace596c4818e7d30fa38aa3b
Adil Faisal <adilfaisal01@proton.me>
2026-06-16 16:00:06 -0400

    feat: add time step parameter and basic kinematic state management to HolonomicMobileRobot

--- lekiwi-robot-base.py
+++ lekiwi-robot-base.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class HolonomicMobileRobot:
-    def __init__(self, num_wheels:int,radius_robots:float,gamma:float,radius_wheels:float ):
+    def __init__(self, num_wheels:int,radius_robots:float,gamma:float,radius_wheels:float, dt:float):
         """Covers holonomic robots, which lekiwi is """
         # n:number of wheels
         # gamma:first wheel angle from base of the robot, use the MJCF definition
@@ -10,20 +10,27 @@ class HolonomicMobileRobot:
         self.R=radius_robots
         self.gamma=gamma
         self.r=radius_wheels
+        self.dt=dt
+        self.state=np.zeros(3)
 
-def MobileRobotBaseKinematics(n:int,R:float,gamma:float, r:float):
+    def mobilerobotkinematics(self):
+        theta_perwheel=2*np.pi/self.n
+        angle_list=[]
+        for i in range(self.n):
+            angle=i*theta_perwheel+self.gamma
+            angle_list.append(angle)
+        sin_list=np.sin(angle_list)
+        cos_list=np.cos(angle_list)
+        #inverse kinematics, LTI matrix
+        A_kin=np.column_stack((sin_list,-cos_list,np.full_like(sin_list, -self.R)))
+        return A_kin, np.linalg.pinv(A_kin)
 
-    """Covers holonomic robots, which lekiwi is """
-    # n:number of wheels
-    # gamma:first wheel angle from base of the robot, use the MJCF definition
-    # R: is the robot’s radius / the distance between the robot’s center and the wheels.
-    theta=2*np.pi/n
-    angle_list=[]
-    for i in range(n):
-        angle=i*theta+gamma
-        angle_list.append(angle)
-    sin_list=np.sin(angle_list)
-    cos_list=np.cos(angle_list)
-    #inverse kinematics, LTI matrix
-    A=np.column_stack((sin_list,-cos_list,np.full_like(sin_list, -R)))
-    return A, np.linalg.pinv(A)
+    def step(self,u_world):
+        theta=self.state[2]
+        c, s= np.cos(theta), np.sin(theta)
+        rot_matrix= np.array([[c,s,0],[-s,c,0],[0,0,1]])
+        A_kinematics, A_pinv_kin=self.mobilerobotkinematics()
+        u_body=rot_matrix@u_world
+        wheel_speeds=
+        
+        
