8dea3d79288c2a41c73c88cbb9cf2255f35bcb1a
Adil Faisal <adilfaisal01@proton.me>
2026-06-18 15:00:03 -0400

    feat(armrobot): add joint_index parameter to homogenous_transform for offset calculation

--- armrobot.py
+++ armrobot.py
@@ -16,13 +16,16 @@ class ArmRobot(Plant):
         A=np.eye(self.num_dof)
         B=self.dt*np.eye(self.num_dof)
         return A,B
-    def _homogenous_transform(self,joint_angle:float,axis:str):
+    def _homogenous_transform(self,joint_angle:float,axis:str, joint_index:int):
         s,c= np.sin(joint_angle), np.cos(joint_angle)
         if axis=='x':
             R=np.array([[1,0,0],[0,c,-s],[0,s,c]])
         elif axis=='y':
             R=np.array([[c,0,s],[0,1,0],[-s,0,c]])
         elif axis=='z':
-            
+            R=np.array([[c,-s,0],[s,c,0],[0,0,1]])
+
+        offset_vector= self.joint_offsets([])
+        
     def forward_kinematics(self,joint_angles:np.ndarray):
         pass
