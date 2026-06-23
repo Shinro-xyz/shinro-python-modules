711f2ec49cbf9bab75cd4eb19fe3223bd628aabe
Adil Faisal <adilfaisal01@proton.me>
2026-06-23 16:30:02 -0400

    feat: add optional initial state estimate to LuenbergerObserver

--- luenberger_observer.py
+++ luenberger_observer.py
@@ -2,7 +2,8 @@ import numpy as np
 from components import StateEstimator
 
 class LuenbergerObserver(StateEstimator):
-    def __init__(self, A:np.ndarray, B:np.ndarray, observer_gain:np.ndarray,C:np.ndarray | None=None, D: np.ndarray | None=None):
+    def __init__(self, A:np.ndarray, B:np.ndarray, observer_gain:np.ndarray,C:np.ndarray | None=None, D: np.ndarray | None=None, x_hat:np.ndarray | None=None, 
+    ):
         self.A=A
         self.B=B
         if C is None:
@@ -11,7 +12,7 @@ class LuenbergerObserver(StateEstimator):
             self.C=C
 
         if D is None:
-            self.D=np.zeros_like(B)
+            self.D=np.zeros((self.C.shape[0], B.shape[1]))
 
         else: 
             self.D=D
