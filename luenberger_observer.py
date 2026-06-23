762a10f4b133370609291cdd0569a7df4e700d28
Adil Faisal <adilfaisal01@proton.me>
2026-06-23 16:00:03 -0400

    feat: implement Luenberger observer with observer gain and default C/D matrices

--- luenberger_observer.py
+++ luenberger_observer.py
@@ -1,5 +1,22 @@
 import numpy as np
 from components import StateEstimator
 
-class LuenBergerObserver(StateEstimator):
-    def __init__(self, A:np.ndarray, B:np.ndarray, C:np.ndarray | None=None, D: np.ndarray | None=None, L
+class LuenbergerObserver(StateEstimator):
+    def __init__(self, A:np.ndarray, B:np.ndarray, observer_gain:np.ndarray,C:np.ndarray | None=None, D: np.ndarray | None=None):
+        self.A=A
+        self.B=B
+        if C is None:
+            self.C=np.eye(A.shape[0])
+        else:
+            self.C=C
+
+        if D is None:
+            self.D=np.zeros_like(B)
+
+        else: 
+            self.D=D
+
+        self.L= observer_gain
+
+    def estimate(self, measurement: np.ndarray, control_input: np.ndarray):
+        
