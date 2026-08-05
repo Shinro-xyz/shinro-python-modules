1b6f096accd62aa1e18013d0a5df245257a2d460
Adil Faisal <adilfaisal01@proton.me>
2026-08-05 17:00:04 -0400

    feat: add configurable initialization arguments to MPPI_Controller

--- src/shinro/controllers/mppi.py
+++ src/shinro/controllers/mppi.py
@@ -5,5 +5,26 @@ import numpy as np
 # optimal control algorithm, sampling based, information theoretic
 
 class MPPI_Controller(Controller):
-    def __init__(self):
-        
+    def __init__(self,
+        dynamics_fn,
+        cost_fn,
+        num_samples:int,
+        temperature:float,
+        dt: float,
+        horizon:int,
+        noise_sigma:np.ndarray,
+        u_min,
+        u_max,
+    ):
+        self.dynamics_fn=dynamics_fn
+        self.cost_fn=cost_fn
+        self.N=horizon
+        self.K=num_samples
+        self.dt=dt
+        self.lam=temperature
+
+        if noise_sigma is None:
+            noise_sigma=np.array([0.5])
+
+        self.noise_sigma=np.asarray(noise_sigma)
+        self.
