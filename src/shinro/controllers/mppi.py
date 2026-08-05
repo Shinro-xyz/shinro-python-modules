484b16915913159bf8610fcdfc91b4a4020ca493
Adil Faisal <adilfaisal01@proton.me>
2026-08-05 18:30:16 -0400

    feat: add MPPI compute implementation and correct horizon/sample assignments

--- src/shinro/controllers/mppi.py
+++ src/shinro/controllers/mppi.py
@@ -19,8 +19,8 @@ class MPPI_Controller(Controller):
     ):
         self.dynamics_fn=dynamics_fn
         self.cost_fn=cost_fn
-        self.N=horizon
-        self.K=num_samples
+        self.N=num_samples
+        self.K=horizon
         self.dt=dt
         self.lam=temperature
 
@@ -31,7 +31,26 @@ class MPPI_Controller(Controller):
         self.u_min=np.asarray(u_min) if u_min is not None else None
         self.u_max=np.asarray(u_max) if u_max is not None else None
 
-        
+        # finding the necessary shapes for making the controller
+        self.D_u= len(self.noise_sigma)
 
-    def compute(self, state, reference=None, **kwargs):
-        raise NotImplementedError("MPPI compute not yet implemented")
+        #nominal control sequence over K steps horizon: (K, D_u) shape
+
+        self.u=np.zeros((self.K, self.D_u))
+
+    def compute(self, x0: np.ndarray):
+
+        D_x=len(x0)
+        # generating gaussian noise
+        epsilon=np.random.normal(loc=0.0,scale=self.noise_sigma, size=(self.N,self.K,self.D_u))
+
+        # adding perturbations to nominal control--> N,K,D_u
+        v=np.expand_dims(self.u, axis=0)+epsilon
+
+        # control limits
+        if self.u_min is not None or self.u_max is not None:
+            v=np.clip(v,self.u_min,self.u_max)
+
+        #parallel N rollouts
+        x_current=np.tile(x0,reps=)
+        
